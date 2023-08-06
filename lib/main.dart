6637f8b4c631b0cead8124141aefc8524710bcc3
Kevin Paul <kevinpaul468@gmail.com>
2023-08-06 23:27:05 +0530

    added transactio.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,50 @@
 import 'package:flutter/material.dart';
+import './transaction.dart';
 
 void main(){
-  runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: true,
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        appBar: AppBar(
-          title: Text("Expense Tracker"),
-          backgroundColor: Colors.blue,
-        ),
-        body: Column(
-          children: <Widget> [
-            Card(
-              child: Container(
-                  width: double.infinity ,
-                  // height: double.infinity,
-                  child: Text("chart- pls holder")
-              ),
+  runApp(MyApp());
+}
 
+class MyApp extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(id: 't1', title: 'product1', amount: 48.69, date: DateTime.now()),
+    Transaction(id: 't2', title: 'product2', amount: 66.53, date: DateTime.now())
+  ];
+ MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return
+        MaterialApp(
+          debugShowCheckedModeBanner: true,
+          home: Scaffold(
+            backgroundColor: Colors.teal,
+            appBar: AppBar(
+              title: Text("Expense Tracker"),
+              backgroundColor: Colors.blue,
             ),
-            Card(
-              child: Container(
-                  width: double.infinity,
-                  // height: double.infinity,
-                  child: Center(
-                      child: Text("list of transactions")
-                  )
-              ),
-            )
-          ],
-        ),
+            body: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget> [
+                Card(
+                  child: Container(
+                      width: double.infinity ,
 
-      ),
-    )
-  );
+                      child: Text("chart- pls holder")
+                  ),
+                ),
+                Column(
+                  children: transactions.map((tx){
+                    return Card(
+                    child: Text(tx.title),
+                    );
+                  }).toList(),
+                ),
+              ],
+            ),
+
+          ),
+
+    );
+  }
 }
