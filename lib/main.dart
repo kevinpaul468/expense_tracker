463228ab86836f12f152d1c79313c4fabe169ea8
Kevin Paul <kevinpaul468@gmail.com>
2023-08-07 06:02:47 +0530

    input button creation started

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import './transaction.dart';
+import 'package:intl/intl.dart';
 
 void main(){
   runApp(MyApp());
@@ -24,19 +25,86 @@ class MyApp extends StatelessWidget {
             ),
             body: Column(
               crossAxisAlignment: CrossAxisAlignment.stretch,
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              // mainAxisAlignment: MainAxisAlignment.start,
               children: <Widget> [
                 Card(
+                  elevation: 7,
                   child: Container(
                       width: double.infinity ,
+                      color: Colors.blue,
 
                       child: Text("chart- pls holder")
                   ),
                 ),
+                Card(
+                  elevation: 5,
+                  child: Container(
+                    padding: EdgeInsets.all(10),
+                    child: Column(
+                      children: [
+                        TextField(
+                            autocorrect: true,
+                          autofocus: true,
+                          decoration: InputDecoration(label: Text("Title")),
+                        ),
+                        TextField(
+                          decoration: InputDecoration(label: Text("Amount")),
+                        ),
+                        TextButton(onPressed: (){
+
+                        },
+                            child: Text("submit",
+                            style: TextStyle(
+                              color: Colors.purpleAccent
+                            ),)
+                        )
+
+                      ],
+                    ),
+                  ),
+                ),
+
                 Column(
                   children: transactions.map((tx){
                     return Card(
-                    child: Text(tx.title),
+                    child: Row(
+                      children: [
+                        Container(
+                          child: Text('₹' +
+                              tx.amount.toString(),
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold ,
+                              fontSize: 20,
+                              color: Colors.red
+                            ),
+
+                          ),
+                          margin: EdgeInsets.symmetric(vertical: 10 , horizontal: 15 ),
+                          decoration: BoxDecoration(border: Border.all(color: Colors.black , width: 2 )),
+                          padding: EdgeInsets.all(10),
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                                tx.title,
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18,
+                                color: Colors.black87
+                              ),
+                            ),
+                            Text(
+                                DateFormat.yMMMMEEEEd().format(tx.date),
+                              style: TextStyle(
+                                color: Colors.grey,
+                                fontWeight: FontWeight.normal
+                              ),
+                            )
+                          ],
+                        )
+                      ],
+                    ),
                     );
                   }).toList(),
                 ),
@@ -44,7 +112,6 @@ class MyApp extends StatelessWidget {
             ),
 
           ),
-
     );
   }
 }
